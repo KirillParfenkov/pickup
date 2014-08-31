fa915aa2be1f6b317b0f8c9ed2624eae481e7db5
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-08-31 13:15:41 +0300

    Fix for minga

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var express = require('express'),
 	serveStatic = require('serve-static'),
 	async = require('async'),
 	bodyParser = require('body-parser'),
-	path = require('path');
+	path = require('path'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
 	UserDao = require('./modules/user-dao'),
 	RoutDao = require('./modules/rout-dao');
 
 // custom
 var userDao = UserDao();
 userDao.connect(function( err ) {
-	if (err) throw err;
+	//if (err) throw err;
 	console.log( 'user dao started!' );
 });
 
 var routDao = RoutDao();
 routDao.connect(function( err ) {
-	if (err) throw err;
+	//if (err) throw err;
 	console.log( 'rout dao started!' );
 });
 
